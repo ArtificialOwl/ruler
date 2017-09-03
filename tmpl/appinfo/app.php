fc55cadc0c2b6343691152aad887fc1c8325b700
Maxence Lange <maxence@artificial-owl.com>
2017-09-03 14:39:21 -0100

    autoload

--- tmpl/appinfo/app.php
+++ tmpl/appinfo/app.php
@@ -1,6 +1,11 @@
 <?php
 
-$app = new \OCA\{{app_namespace}}\AppInfo\Application();
+
+namespace OCA\{{app_namespace}}\AppInfo;
+
+require_once __DIR__ . '/autoload.php';
+
+$app = new Application();
 
 $app->registerNavigation();
 $app->registerSettingsAdmin();
---
+++ tmpl/appinfo/autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OCA\{{app_namespace}}\AppInfo;
+
+$composerDir = __DIR__ . '/../vendor/';
+
+// Uncomment is using composer
+//if (is_dir($composerDir) && file_exists($composerDir . 'autoload.php')) {
+//	require_once $composerDir . 'autoload.php';
+//}
+
